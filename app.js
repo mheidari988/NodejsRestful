0fcd6618463ee1aad33f959ed9e854117f046528
Reza Heidari <reza.heidari@amplicade.com>
2023-01-16 23:46:12 +0300

    init eslint

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const assert = require('assert');
+const express = require("express");
+const assert = require("assert");
 
-const port = process.env.PORT;
-assert(port, 'Port is not defined.');
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || 3000;
+assert(port, "Port is not defined.");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('hello from express');
+app.get("/", (req, res) => {
+    res.send("hello from express");
 });
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
-})
+});
