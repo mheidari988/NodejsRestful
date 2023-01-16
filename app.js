57ca2e270dd0117fdc316fbd7b66332ce4955fef
Reza Heidari <reza.heidari@amplicade.com>
2023-01-17 00:10:50 +0300

    insert some dummy data

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const port = process.env.PORT || 3000;
 assert(port, "Port is not defined.");
 
 const app = express();
+const bookRouter = express.Router();
+
+bookRouter.route("/books")
+    .get((req, res) => {
+        const response = {
+            hello: "This is my API"
+        };
+        res.json(response);
+    });
+
+app.use("/api", bookRouter);
 
 app.get("/", (req, res) => {
     res.send("hello from express");
