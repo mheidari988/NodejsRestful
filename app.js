e15a1b1f3e7260b8670d39a61fef3eb6287a0a95
Reza Heidari <reza.heidari@amplicade.com>
2023-01-17 01:09:34 +0300

    add mongodb

--- app.js
+++ app.js
@@ -6,18 +6,17 @@ const app = express();
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+    res.send("hello from express");
+});
+
 bookRouter.route("/books")
     .get((req, res) => {
         res.json({
             title: "hello"
         });
     });
-
 app.use("/api", bookRouter);
-
-app.get("/", (req, res) => {
-    res.send("hello from express");
-});
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
